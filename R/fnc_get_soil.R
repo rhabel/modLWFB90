09e741f6e974988eb89715d15331f841a61356a3
rhabel <raphael.habel@hotmail.de>
2020-12-14 11:21:18 +0100

    new function names applied

--- R/fnc_get_soil.R
+++ R/fnc_get_soil.R
@@ -28,6 +28,9 @@
 #'
 #' @return Returns a list of soil data frames completely processed to be further used in \code{\link[LWFBrook90R]{msiterunLWFB90}} or \code{\link[LWFBrook90R]{runLWFB90}}
 #'
+#' @import doParallel
+#' @import foreach
+#'
 #' @example inst/examples/fnc_get_soil_ex.R
 #' @export
 
@@ -51,9 +54,8 @@ fnc_get_soil <- function(df.ids,
 
   dgm.stack <- raster::stack(list.files(input_paul, pattern = "aspect.sdat|slope.sdat", full.names=T))
   df.dgm <- cbind("ID" = df.ids$ID,
-                  as.data.frame(fnc_extract_points(lay = dgm.stack,
-                                                   xy = xy_gk,
-                                                   buffering = T)))
+                  as.data.frame(fnc_extract_points_dgm(lay = dgm.stack,
+                                                   xy = xy_gk)))
 
   # initialise list
   ls.soils <- vector("list", length = nrow(df.ids))
