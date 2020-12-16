4ebcd779ff1aec3857d41f63df872e20d9e4c23a
Müller <Ann-Christin.Mueller@forst.bwl.de>
2020-12-16 14:40:37 +0100

    nFK is calculated for 1 m soil depth without humus layer.

--- R/fnc_AWC.R
+++ R/fnc_AWC.R
@@ -4,7 +4,7 @@
 #'This function calculates the plant available water content (AWC, nutzbare Feldkapazitaet) from the Mualem-van Genuchten retention curve unsing \code{\link{fnc_MvG.swc}}. The function calculates the AWC per soil layer or for the whole depth at once. The gravel content is already taken into account.
 #'
 #' @param data is the list of soil properties calculated by \code{\link{fnc_get_soil}}
-#' @param nFK indicates whether the AWC is calculated for the whole soil depth (\code{nFK = T}), returning a vector with one vector element for each list element. \code{nFK = F} returns the initial list with the additional columns: \code{fc} (vol. water content at 63 hPa), \code{pwp} (vol. water content at 15000 hPa) and \code{AWC} (AWC in mm per m^2 and depth of the soil layer)
+#' @param nFK indicates whether the AWC is calculated for t1 m soil depth (\code{nFK = T}), returning a vector with one vector element for each list element. \code{nFK = F} returns the initial list with the additional columns: \code{fc} (vol. water content at 63 hPa), \code{pwp} (vol. water content at 15000 hPa) and \code{AWC} (AWC in mm per m^2 and depth of the soil layer)
 #'
 #' @return Returns either the expanded input list of soil properties or a vector containing the AWC for the whole soil depth with one vector element per input list element
 
@@ -28,14 +28,6 @@ fnc_AWC <- function(data, nFK = F) {
 
     nFK <- c()
     for(i in c(1: length(data))){
-      sum <- sum(data[[i]]$AWC)
-      sum <- if(min(data[[i]]$lower) > -1.0){
-                sum(data[[i]]$AWC[data[[i]]$lower >= -1.0]) * min(data[[i]]$lower)*(-1)
-             }
-             else{
-               sum(data[[i]]$AWC[data[[i]]$lower >= -1.0])
-             }
-      sum <- sum(data[[i]]$AWC[data[[i]]$lower >= -1.0])
       sum <- sum(data[[i]]$AWC[data[[i]]$lower >= -1.0 & data[[i]]$upper <= 0])
       nFK <- append(nFK, sum)
     }
