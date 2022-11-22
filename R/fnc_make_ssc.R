ef99ce0e2d93a48df0d050b7496105abd30039ec
Habel <Raphael.Habel@forst.bwl.de>
2022-11-22 13:04:58 +0100

    minor bugs

--- R/fnc_make_ssc.R
+++ R/fnc_make_ssc.R
@@ -16,14 +16,14 @@
 fnc_make_ssc <- function(texture){
 
   if (!texture %in% as.data.frame(soiltexture::TT.classes.tbl("DE.BK94.TT"))[,1] ){
-    stop("Error. Please provide valid soil texture class... See ?make_ssc")
+    stop("Error. Please provide valid soil texture class... See ?fnc_make_ssc")
   }
 
   sicl <-  case_when(texture == "Ss" ~  c(5,    2.5),
                      texture == "Su2" ~ c(17.5, 2.5),
                      texture == "Sl2" ~ c(17.5, 6.5),
                      texture == "Sl3" ~ c(25,    10),
-                     texture == "St2" ~ c(5,   11.5),
+                     texture == "St2" ~ c(5,   11.0),
                      texture == "Su3" ~ c(32.5,   4),
                      texture == "Su4" ~ c(45,     4),
                      texture == "Slu" ~ c(45,  12.5),
@@ -36,20 +36,20 @@ fnc_make_ssc <- function(texture){
                      texture == "Lts" ~ c(22.5,  35),
                      texture == "Ts4" ~ c(7.5,   30),
                      texture == "Ts3" ~ c(7.5,   40),
-                     texture == "Uu" ~  c(90,     4),
+                     texture == "Uu" ~  c(88.2, 3.7),
                      texture == "Us" ~  c(65,     4),
-                     texture == "Ut2" ~ c(78.5,  10),
-                     texture == "Ut3" ~ c(76.5,14.5),
+                     texture == "Ut2" ~ c(77.5, 9.9),
+                     texture == "Ut3" ~ c(76  ,14.4),
                      texture == "Uls" ~ c(57.5,12.5),
-                     texture == "Ut4" ~ c(74,    21),
+                     texture == "Ut4" ~ c(72.2,20.6),
                      texture == "Lu" ~  c(57.5,23.5),
                      texture == "Lt3" ~ c(40,    40),
-                     texture == "Tu3" ~ c(57.5,37.5),
-                     texture == "Tu4" ~ c(70,    30),
+                     texture == "Tu3" ~ c(56.3,36.3),
+                     texture == "Tu4" ~ c(68.3,28.3),
                      texture == "Ts2" ~ c(7.5,   55),
                      texture == "Tl" ~  c(22.5,  55),
-                     texture == "Tu2" ~ c(42.5,  55),
-                     texture == "Tt" ~  c(17.5,82.5),
+                     texture == "Tu2" ~ c(38.6,52.8),
+                     texture == "Tt" ~  c(11.7,76.7),
                      T ~ c(NA_real_, NA_real_)
   )
   sasicl <- c(100-sum(sicl), sicl)
