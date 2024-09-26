a2571ac49dd2abfe08e4dbf98f5a7802639eabbb
Habel <Raphael.Habel@forst.bwl.de>
2024-09-26 12:39:01 +0200

    minor bugs in agg-bg-functions

--- R/PrecDailyToYearly.R
+++ R/PrecDailyToYearly.R
@@ -15,8 +15,12 @@ Prec.DailyToYearly <- function(dat) {
   setDT(dat)
   setkey(dat, YR)
 
+  dat[, PREC := RFAL + SFAL]
+  dat[, BSTN := RFAL + SFAL - RINT - SINT]
+
   dat_yr <- dat[,list(
-    PREC = sum(PREC)),
+    PREC = sum(PREC),
+    BSTN = sum(BSTN)),
   by = YR]
   return(dat_yr)
 
